33017f7a4e363516a41278885be7bbae7c67209b
Fébert András <andris.febert111@gmail.com>
2025-10-31 13:28:48 +0100

    repo létrehozása

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<List<int>> matrix = 
+        [
+            [1,2,3,4,5,6], 
+            [7,8,9,10,11]
+        ];
+        
+        var ertek = matrix[0][2];
+
+        Console.WriteLine(ertek);
+    }
+}
