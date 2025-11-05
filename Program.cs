ac51a70c38470d7a640cca36830bcad0b1d9b618
Fébert András <andris.febert111@gmail.com>
2025-11-05 22:04:32 +0100

    szépítés

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ class Program
         int x = 5;
         int y = 10;
         
-        int fegyver = 0;
+        int fegyver_van = 0;
         int hp = 60;
 
         string bekeres = " ";
@@ -28,7 +28,7 @@ class Program
 
             if (terkep[y + 1][x] != FAL)
             {
-                iranyok.Add(("dél"));
+                iranyok.Add("dél");
             }
 
             if (terkep[y][x - 1] != FAL)
@@ -38,7 +38,7 @@ class Program
 
             if (terkep[y][x + 1] != FAL)
             {
-                iranyok.Add(("kelet"));
+                iranyok.Add("kelet");
             }
 
             Console.WriteLine("\t \t \t \t \t \t hp: " + hp);
@@ -107,7 +107,7 @@ class Program
             
             
 
-            if (terkep[y][x] == E)
+            if (terkep[y][x] == ELLENSEG)
             {
                 hp -= 20;
 
@@ -121,12 +121,12 @@ class Program
                     Console.WriteLine("Talált egy csapdát és vesztett 20 hp-t!");
                 }
             } 
-            else if (terkep[y][x] == F && fegyver == 0)
+            else if (terkep[y][x] == FEGYVER && fegyver_van == 0)
             {
                 Console.WriteLine("Talált egy kardot!");
-                fegyver++;
+                fegyver_van++;
             }
-            else if (terkep[y][x] == B)
+            else if (terkep[y][x] == BOSS)
             {
                 Console.WriteLine("Megtalálta a kijáratot, amit egy nagy alvó patkány őrzött! Megpróbálja legyőzni?");
                 Console.Write("[I]gen [N]em ");
@@ -135,7 +135,7 @@ class Program
                 switch (valasz)
                 {
                     case "i":
-                        if (fegyver == 1)
+                        if (fegyver_van == 1)
                         {
                             Console.WriteLine("Sikeresen legyőzte a szörnyet és kijutott a labirintusból! Gratulálok!");
                             bekeres = "kilépés";
@@ -191,31 +191,31 @@ class Program
 
     public const int FAL = 1;
     public const int UT = 0;
-    public const int E = 2; //enemy
-    public const int F = 3; //fegyver
-    public const int B = 4; //boss
+    public const int ELLENSEG = 2; //enemy
+    public const int FEGYVER = 3; //fegyver
+    public const int BOSS = 4; //boss
     
     static void Main(string[] args)
     {
         Console.WriteLine("Üdvözöllek a játékban! \n" +
-                          "A kilépésher írja be, hogy ""kilépés");
+                          "A kilépésher írja be, hogy 'kilépés'");
 
         
         var terkep = new List<List<int>>
         {
             //       0   1   2   3   4   5  6   7   8   9   10  11
-            new() { FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL},   // 0
-            new() { FAL,FAL,FAL,FAL,FAL,B,FAL,FAL,FAL,FAL,FAL,FAL},     // 1
-            new() { FAL,FAL,FAL,UT,UT,UT,FAL,UT,UT,E,FAL,FAL},          // 2
-            new() { FAL,UT,UT,UT,FAL,UT,FAL,UT,FAL,FAL,FAL,FAL},        // 3
-            new() { FAL,UT,FAL,UT,FAL,E,FAL,UT,FAL,F,UT,FAL},           // 4
-            new() { FAL,E,FAL,UT,FAL,FAL,FAL,UT,FAL,FAL,UT,FAL},        // 5
-            new() { FAL,FAL,FAL,UT,FAL,UT,UT,UT,UT,FAL,UT,FAL},         // 6
-            new() { FAL,UT,UT,UT,UT,UT,FAL,FAL,UT,FAL,UT,FAL},          // 7
-            new() { FAL,UT,FAL,FAL,FAL,UT,FAL,FAL,UT,UT,UT,FAL},        // 8
-            new() { FAL,E,FAL,UT,UT,UT,FAL,FAL,E,FAL,UT,FAL},           // 9
-            new() { FAL,FAL,FAL,FAL,FAL,UT,FAL,FAL,FAL,FAL,E,FAL},      // 10
-            new() { FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL}    // 11
+            new() { FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL},       // 0
+            new() { FAL,FAL,FAL,FAL,FAL,BOSS,FAL,FAL,FAL,FAL,FAL,FAL},      // 1
+            new() { FAL,FAL,FAL,UT,UT,UT,FAL,UT,UT,ELLENSEG,FAL,FAL},       // 2
+            new() { FAL,UT,UT,UT,FAL,UT,FAL,UT,FAL,FAL,FAL,FAL},            // 3
+            new() { FAL,UT,FAL,UT,FAL,ELLENSEG,FAL,UT,FAL,FEGYVER,UT,FAL},  // 4
+            new() { FAL,ELLENSEG,FAL,UT,FAL,FAL,FAL,UT,FAL,FAL,UT,FAL},     // 5
+            new() { FAL,FAL,FAL,UT,FAL,UT,UT,UT,UT,FAL,UT,FAL},             // 6
+            new() { FAL,UT,UT,UT,UT,UT,FAL,FAL,UT,FAL,UT,FAL},              // 7
+            new() { FAL,UT,FAL,FAL,FAL,UT,FAL,FAL,UT,UT,UT,FAL},            // 8
+            new() { FAL,ELLENSEG,FAL,UT,UT,UT,FAL,FAL,ELLENSEG,FAL,UT,FAL}, // 9
+            new() { FAL,FAL,FAL,FAL,FAL,UT,FAL,FAL,FAL,FAL,ELLENSEG,FAL},   // 10
+            new() { FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL,FAL}        // 11
         };
 
         Mozgas(terkep);
